bc9cd4e4d02f7e349f2413b9d10bad24b2a0e447
Zig1375 <zig@zig.zag>
2016-06-02 16:06:53 +0300

    fix

--- Sources/SwiftEmailSender/Helper/StringUtils.swift
+++ Sources/SwiftEmailSender/Helper/StringUtils.swift
@@ -1,7 +1,23 @@
+/**
+* Copyright IBM Corporation 2015
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
 #if os(OSX) || os(iOS)
-    import Darwin
+import Darwin
 #elseif os(Linux)
-    import Glibc
+import Glibc
 #endif
 
 import Foundation
@@ -19,12 +35,7 @@ public class StringUtils {
     ///
     public static func toUtf8String(_ str: String) -> NSData? {
         let nsstr:NSString = str.bridge()
-        #if os(Linux)
-        let data = nsstr.dataUsingEncoding(NSUTF8StringEncoding)
-        #else
-        let data = nsstr.data(using: NSUTF8StringEncoding)
-        #endif
-        return data
+        return nsstr.data(using: NSUTF8StringEncoding)
     }
 
 
@@ -37,14 +48,8 @@ public class StringUtils {
     ///
     public static func toNullTerminatedUtf8String(_ str: String) -> NSData? {
         let nsstr:NSString = str.bridge()
-        #if os(Linux)
-        let cString = nsstr.cStringUsingEncoding(NSUTF8StringEncoding)
-        #else
         let cString = nsstr.cString(using: NSUTF8StringEncoding)
-        #endif
-
-        let data = NSData(bytes: cString, length: Int(strlen(cString!))+1)
-        return data
+        return NSData(bytes: cString, length: Int(strlen(cString!))+1)
     }
 
 
@@ -70,7 +75,7 @@ public class StringUtils {
 // operating systems.
 //
 #if os(OSX) || os(iOS)
-    
+
 public extension String {
     func bridge() -> NSString {
         return self as NSString
